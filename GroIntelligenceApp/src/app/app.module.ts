beadb78c463d8f3bc32af3fd9b5375b40e981118
Dannee Kim <kim.dannee@gmail.com>
2019-12-14 15:37:14 -0500

    Creating Navigation header component

--- GroIntelligenceApp/src/app/app.module.ts
+++ GroIntelligenceApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationHeaderComponent } from './navigation-header/navigation-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationHeaderComponent
   ],
   imports: [
     BrowserModule,
