2b0f5818645971038748a30bae92af6673b5c94b
Dannee Kim <kim.dannee@gmail.com>
2019-12-15 02:17:16 -0500

    Consuming Industry labels from content.json

--- GroIntelligenceApp/src/app/industry-link/industry-link.component.ts
+++ GroIntelligenceApp/src/app/industry-link/industry-link.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-industry-link',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndustryLinkComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private httpService: HttpClient
+  ) { }
+  groData: Array<any>;
+  industryList: Array<any>;
+  industryObject: Object = {};
+  industryLabel1: string = "";
+  industryLabel2: string = "";
+  industryLabel3: string = "";
+  industryLabel4: string = "";
 
   ngOnInit() {
+    this.httpService.get('../../assets/data/content.json').subscribe(
+      data => {
+        this.groData = data as any [];
+        this.groData.forEach( element => {
+          if(element.label === 'Industries'){
+            this.industryList = element.subnavList;
+          }
+        })
+        if(this.industryList != null){
+          this.industryLabel1 = this.industryList[0].label;
+          this.industryLabel2 = this.industryList[1].label;
+          this.industryLabel3 = this.industryList[2].label;
+          this.industryLabel4 = this.industryList[3].label;
+        }
+      },
+      (err: HttpErrorResponse) => {
+        console.log (err.message);
+      }
+    );
   }
+  
 
 }
