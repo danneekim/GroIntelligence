3a9228745c8cf3910ff39312d4accaf0042787c9
Dannee Kim <kim.dannee@gmail.com>
2019-12-15 23:11:19 -0500

    adding path-reroute to /products

--- GroIntelligenceApp/src/app/app-routing.module.ts
+++ GroIntelligenceApp/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { ProductLinkComponent } from './product-link/product-link.component';
 import { IndustryLinkComponent } from './industry-link/industry-link.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: '/products', pathMatch:'full'}, 
   {path: 'products', component: ProductLinkComponent, data:{shouldDetach: true}},
   {path: 'industries', component: IndustryLinkComponent, data:{shouldDetach: true}}
 ];
